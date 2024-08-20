3a212814fa9d0e40a6b8337fdaa2c262d24ae64b
antreaslamprou <146127103+antreaslamprou@users.noreply.github.com>
2024-08-20 21:04:07 +0300

    Add files via upload

---
+++ Arkanoid/game.py
@@ -0,0 +1,260 @@
+# Arkanoid.py
+# Author: Antreas Lamprou (G21070510)
+# Email: alambrou1@uclan.ac.uk
+# Description: The Arkanoid.py program demonstrates the classic Arkanoid game where a ball bounces to the sides and on
+# the craft giving points when the bricks brake. Once all the bricks break, the user wins while if the ball touches
+# the ground, the user loses. The game has 2 Modes : Easy and Hard.
+
+
+# import library for graphics
+from tkinter import *
+
+
+# class that gives brick properties and creates them when called
+class Bricks:
+    def __init__(self, x1, y1, x2, y2, color, outline):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.color = color
+        self.outline = outline
+        self.object = canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline=outline)
+
+
+# class that creates the ball and adds speed properties
+class Ball:
+
+    # will be used to change ball's speed and position
+    def __init__(self, x, y, speed_x, speed_y, radius, color):
+        self.x = x
+        self.y = y
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+        self.radius = radius
+        self.color = color
+
+    # changes ball position based on where it hits
+    def move(self):
+        self.x = self.x + self.speed_x
+        if self.x >= WIDTH - self.radius:
+            self.speed_x = -abs(self.speed_x)
+        if self.x <= self.radius:
+            self.speed_x = abs(self.speed_x)
+        self.y = self.y + self.speed_y
+        if self.y <= self.radius:
+            self.speed_y = abs(self.speed_y)
+
+
+# creates the craft as a class
+class Craft:
+
+    def __init__(self, x, y, x_size, y_size, color):
+        self.x = x
+        self.y = y
+        self.x_size = x_size
+        self.y_size = y_size
+        self.color = color
+        self.object = canvas.create_rectangle(x-r_width, y-r_height, x+r_width, y+r_height ,fill=color)
+
+    def draw(self):
+        canvas.coords(self.object, self.x-self.x_size, self.y-self.y_size, self.x+self.x_size, self.y+self.y_size)
+
+    def move(self, mouse_x):
+        self.x = mouse_x
+        if mouse_x <= 60:
+            self.x = 60
+        if mouse_x >= WIDTH - 60:
+            self.x = WIDTH - 60
+
+
+# creates score as a class
+class Score:
+    def __init__(self, text, font, fill):
+        self.text = text
+        self.font = font
+        self.fill = fill
+        self.text_id = canvas.create_text(50, HEIGHT - 20, text= self.text, font= self.font, fill= self.fill)
+
+    # updates the score
+    def update(self, score):
+        canvas.itemconfig(self.text_id, text='Score: ' + str(score), font=('System', 12), fill='black')
+
+
+# draw and move the craft
+def draw_and_move_craft(event):
+    rec.move(event.x)
+    rec.draw()
+
+
+# creates a function to change the ball speed and position based on the colligion with the craft
+def colligion_ball_craft():
+    global b, rec, DEFAULT_BALL_RADIUS, r_height, r_width, HEIGHT
+    if  HEIGHT - 35 >= b.y + DEFAULT_BALL_RADIUS >= HEIGHT - r_height - 35:
+        if rec.x - r_width <= b.x <= rec.x + r_width:
+            b.speed_x = (b.x - rec.x) * 10 / r_width  # adjust speed (x-axis) based on the the point of CONTACT between Ball and Craft
+            b.speed_y = -abs(b.speed_y)  # reverse the ball to go up
+
+
+# creates a function to change the ball speed and position based on the colligion with the bricks
+def colligion_ball_bricks():
+    global b, DEFAULT_BALL_RADIUS, score, bricks, side_x, side_y
+    for brick in bricks:
+        if b.y - DEFAULT_BALL_RADIUS <= brick.y2:
+            if brick.x1 <= b.x + DEFAULT_BALL_RADIUS <= brick.x2 or brick.x1 <= b.x - DEFAULT_BALL_RADIUS <= brick.x2:
+                bricks.remove(brick)
+                canvas.delete(brick.object)
+                b.speed_y = abs(b.speed_y)
+                score += 10
+                if brick.y1 <= b.y - DEFAULT_BALL_RADIUS < brick.y2:
+                    if b.speed_x > 0:
+                        b.speed_x = -b.speed_x
+                    else:
+                        b.speed_x = abs(b.speed_x)
+
+
+# after the user loses the end screen appears
+def end():
+    canvas.delete(ball_id)
+    canvas.create_rectangle(250, HEIGHT/2 - 100, WIDTH-250, HEIGHT / 2 + 100, fill='cyan', width=10)
+    canvas.create_text(WIDTH/2, HEIGHT/2-25, text='GAME OVER', font=('System', 30), fill='red')
+    canvas.create_text(WIDTH/2, HEIGHT/2+20, text='SCORE: ' + str(score), font=('System', 20), fill='red')
+
+
+# after the user wins the win screen appears
+def won():
+    canvas.delete(ball_id)
+    canvas.create_rectangle(250, HEIGHT / 2 - 100, WIDTH - 250, HEIGHT / 2 + 100, fill='cyan', width=10)
+    canvas.create_text(WIDTH/2, HEIGHT/2-25, text='YOU WON', font=('System', 30), fill='green')
+    canvas.create_text(WIDTH/2, HEIGHT/2+25, text='SCORE: ' + str(score), font=('System', 20), fill='green')
+
+
+# creates a function for the animation of the ball, the ball's colligions and update the score
+def animation():
+    global b, bricks, DEFAULT_BALL_RADIUS, HEIGHT
+    b.move()
+    canvas.coords(ball_id, b.x-b.radius, b.y-b.radius, b.x+b.radius, b.y+b.radius)
+    colligion_ball_craft()
+    colligion_ball_bricks()
+    s1.update(score)
+    if b.y + DEFAULT_BALL_RADIUS == HEIGHT:
+        end()
+    if len(bricks) == 0:
+        won()
+    canvas.after(DELAY, animation)
+
+
+# creates the 5 ball shadow illusion
+def shadow():
+    global WIDTH, HEIGHT, DEFAULT_BALL_RADIUS, b
+    balls.append(canvas.create_oval(b.x -DEFAULT_BALL_RADIUS, b.y -DEFAULT_BALL_RADIUS, b.x + DEFAULT_BALL_RADIUS, b.y + DEFAULT_BALL_RADIUS, fill='dark red'))
+    if len(balls) > MAX_BALLS:
+        canvas.delete(balls.pop(0))
+    canvas.tag_raise('ball')
+    canvas.after(DELAY, shadow)
+
+
+# creates the game mode based on input
+def key_press(event):
+    if event.char == 'x' or event.char == 'X':
+        quit()
+    if event.char == '1':
+        canvas.delete(text1,text2,text3,text4,main_menu,box)
+        animation()
+        shadow()
+    if event.char == '2':
+        canvas.delete(text1,text2,text3,text4,main_menu,box)
+        animation()
+        animation()
+        animation()
+        shadow()
+
+
+# use some standard variables.
+WIDTH = 800
+HEIGHT = 600
+Title = "Arkanoid - First Capstone Project"
+color = ["purple" , "blue" , "skyblue", "yellow", "orange"] # brick color
+side_x = 53 # brick width side size
+side_y = 20 # brick height side size
+
+
+# for the ball
+DELAY = 20  # delay between animations, in milliseconds
+DEFAULT_SPEED = 5
+DEFAULT_BALL_RADIUS = 10
+
+
+# for moving rectangle's position and size
+x = WIDTH/2
+y = HEIGHT - 50
+r_height = 5
+r_width = 50
+
+
+#create the window
+win = Tk()
+win.title(Title)
+win.geometry(str(WIDTH + 10)+"x"+str(HEIGHT+10))
+
+
+# makes the window not resizeable
+win.resizable(False, False) # [https://www.tutorialspoint.com/how-to-make-a-tkinter-window-not-resizable#:~:text=Let%20us%20suppose%20that%20we,resizable(False%2C%20False).]
+
+
+# will be used for the shadow of ball
+balls = []
+MAX_BALLS = 5
+
+
+# create canvas and frame
+canvas = Canvas(win, width=WIDTH, height=HEIGHT)
+canvas.pack()
+
+
+# create custom background
+b = 100
+for i in range (0,HEIGHT,b):
+    for j in range (0,WIDTH,b):
+        canvas.create_rectangle(j, i,j+b, i+b, fill= 'pink', outline='white')
+canvas.create_rectangle(2,0, WIDTH, HEIGHT)
+
+
+# creates the bricks and save them in an array
+bricks = []
+for i in range(0,5):
+    for j in range (0,15):
+        bricks.append(Bricks(j * side_x + 5, i*30, j * side_x + side_x - 1, i*30 + 3 + side_y, color[i], color[i]))
+
+
+# creates the first craft at the central position
+rec = Craft(WIDTH / 2, HEIGHT - r_height - 35, r_width, r_height, 'black')
+
+
+# creates the score
+score = 0
+s1 = Score('Score: ', ('System', 12), 'black')
+
+
+# creates the ball
+b = Ball(WIDTH / 2, HEIGHT / 2, 0, DEFAULT_SPEED, DEFAULT_BALL_RADIUS, 'red')
+ball_id = canvas.create_oval(b.x - b.radius, b.y - b.radius, b.x + b.radius, b.y + b.radius, fill='red', outline='black', tags='ball')
+
+
+# creates main menu
+main_menu= canvas.create_rectangle(5,145,WIDTH-3,HEIGHT-3,fill= 'pink', outline='pink')
+box = canvas.create_rectangle(213,25,587,119, fill='pink', outline = 'black', width= 2)
+text1= canvas.create_text(WIDTH/2, 71, text= "ARKANOID", font= ('System', 80), fill= 'black')
+text2= canvas.create_text(WIDTH/2, 250, text="Easy Mode - Press: 1",font= ('System', 40), fill= 'green')
+text3= canvas.create_text(WIDTH/2, 340, text="Hard Mode - Press: 2",font= ('System', 40), fill= 'red')
+text4= canvas.create_text(WIDTH/2, 510, text="To exit press: x",font= ('System', 30), fill= 'black')
+
+
+# binds functions
+win.bind('<KeyPress>', key_press)
+
+win.bind('<Motion>', draw_and_move_craft)
+
+
+# keeps the window up
+win.mainloop()
---
+++ Hulk Run/game.py
@@ -0,0 +1,320 @@
+# alambrou1_CO1417.py
+# Author: Antreas Lamprou (G21070510)
+# Email: alambrou1@uclan.ac.uk
+# Description: The Dino.py program demonstrates the classic game where a dragon has to jump over some trees that are
+# coming its way. This has been modified to a hulk jumping over buildings so he will avoid breaking them.
+
+# import library for graphics
+from tkinter import *
+from random import *
+
+
+# function that updates everything (dino/hulk animation, score, cactus/buildings, color of sky, clouds/airplanes and colligions)
+def update_all():
+    global frame_index, score
+    if not paused and not beginning:
+        if not dead:
+            if not in_a_jump:
+                canvas.itemconfig(dino_animation, image=frames[frame_index])
+                frame_index += 1
+                if frame_index == FRAME_COUNT:
+                    frame_index = 0
+            else:
+                canvas.itemconfig(dino_animation, image=frames[FRAME_COUNT])
+
+            (x, y) = canvas.coords(ground)  # this picks the coordinates of the given object
+            if x >= 0:
+                canvas.move(ground, -20, 0)  # move the background to the left by changing the X by -20
+            else:
+
+                canvas.move(ground, WIDTH, 0)
+
+            update_score()
+            cactus()
+            color_sky()
+            cloud()
+            colligion()
+
+    win.after(DELAY, update_all)
+
+
+# function to make the jump
+def update(index):
+    global in_a_jump, jump_index
+    if not paused:
+        if not dead and not beginning:
+            canvas.itemconfig(dino_animation, image=frames[index])
+
+            # if in a jump, move the character by jump_offset
+            if in_a_jump:
+                jump_offset = jump_offsets[jump_index]
+                canvas.move(dino_animation, 0, jump_offset)  # the 'canvas.move' function moves the specified object by the specified X.Y offset
+                jump_index = jump_index + 1  # prepare for the next phase of the jump
+                if jump_index > len(jump_offsets) - 1:  # when the jump ends, reset the jump_index and set in_a_jump back to False
+                    jump_index = 0
+                    in_a_jump = False
+
+            index += 1
+            if index == FRAME_COUNT:
+                index = 0
+
+    win.after(DELAY, update, index)
+
+
+# function to avoid double jumping
+def jump(__self__):
+    global in_a_jump
+    if not dead and not paused:
+        if not in_a_jump:  # only process the jump event if no other jump is in progress
+            in_a_jump = True
+
+
+# function to make the colligion between dino/hulk & cactus/buildings
+def colligion():
+    (h_x,h_y) = canvas.coords(dino_animation)
+    (c1_x,c1_y) = canvas.coords(cactus1)
+    (c2_x,c2_y) = canvas.coords(cactus2)
+    (c3_x,c3_y) = canvas.coords(cactus3)
+    d1_x = abs(h_x - c1_x)
+    d1_y = abs(h_y - c1_y)
+    d2_x = abs(h_x - c2_x)
+    d2_y = abs(h_y - c2_y)
+    d3_x = abs(h_x - c3_x)
+    d3_y = abs(h_y - c3_y)
+    if d1_x <= 15 and d1_y <= 40 :
+        lost()
+        canvas.move(cactus1, 0, HEIGHT+100)
+        canvas.coords(rip1, c1_x,c1_y)
+    if d2_x <= 25 and d2_y <= 40 :
+        lost()
+        canvas.move(cactus2, 0, HEIGHT + 100)
+        canvas.coords(rip2, c2_x, c2_y)
+    if d3_x <= 15 and d3_y <= 40 :
+        lost()
+        canvas.move(cactus3, 0, HEIGHT + 100)
+        canvas.coords(rip3, c3_x, c3_y)
+
+
+# function for the update of score and highscore
+def update_score():
+    global score, highscore, speed
+    score += 1
+    canvas.itemconfig(s_text, text=f'{score:08}')
+    if highscore <= score:
+        highscore = score
+    canvas.itemconfig(h_text, text=f'HI {highscore:08}')
+
+
+# function for the movement of cactus/buildings
+def cactus():
+    canvas.move(cactus1,-speed, 0)
+    canvas.move(cactus2,-speed, 0)
+    canvas.move(cactus3, -speed, 0)
+    (x1, y1) = canvas.coords(cactus1)
+    (x2, y2) = canvas.coords(cactus2)
+    (x3, y3) = canvas.coords(cactus3)
+    if x1 < -20:
+        canvas.move(cactus1, randint(WIDTH + 20, WIDTH + 500), 0)
+    if x2 < -20:
+        canvas.move(cactus2, randint(WIDTH + 20, WIDTH + 500), 0)
+    if x3 < -20:
+        canvas.move(cactus3, randint(WIDTH + 20, WIDTH + 500), 0)
+
+
+# function for movement of clouds/airplanes
+def cloud():
+    canvas.move(airplane1, -speed/2, 0)
+    (x1,y1) = canvas.coords(airplane1)
+    (x2, y2) = canvas.coords(airplane2)
+    if x1 < -50:
+        canvas.move(airplane1, randint(WIDTH + 20, WIDTH + 500), 0)
+    canvas.move(airplane2, speed/2, 0)
+    if x2 > WIDTH + 50:
+        canvas.move(airplane2, -randint(WIDTH + 20, WIDTH + 500), 0)
+
+
+# fucntion to change the colour of the sky
+def color_sky():
+    j = int(str(score)[-2:])
+    if 00 <= j <= 25:
+        i = 0
+    elif 26 <= j <= 50:
+        i = 1
+    elif 51 <= j <= 75:
+        i = 2
+    elif 76 <= j < 100:
+        i = 3
+    canvas.itemconfig(sky_box, fill= sky[i])
+
+
+# function to make the losing screen
+def lost():
+    global dead
+    dead = True
+    canvas.itemconfig(dino_animation, image=dead_frame)
+    canvas.move(dino_animation, -70,0)
+    canvas.coords(d_box, WIDTH / 2 - 150, HEIGHT / 2 - 50, WIDTH / 2 + 150, HEIGHT / 2 + 50)
+    canvas.coords(d_text, WIDTH / 2, HEIGHT / 2)
+
+
+# function to do actions based on user inputs
+def key_press(event):
+    global paused, score, dead, beginning, in_a_jump, jump_index
+    if event.char == 'q' or event.char == 'Q':
+        quit()
+    if event.char == 'p' or event.char == 'P':
+        if paused == False:
+            paused = True
+            canvas.coords(box, WIDTH/2 - 150, HEIGHT/2-50, WIDTH/2 + 150, HEIGHT/2 + 50)
+            canvas.coords(text, WIDTH/2, HEIGHT/2)
+        else:
+            paused = False
+            canvas.coords(box, WIDTH+5, HEIGHT+5, WIDTH+10, HEIGHT+10)
+            canvas.coords(text, WIDTH + 50, HEIGHT+ 50)
+    if event.char == 'r' or event.char == 'R':
+        if dead == True:
+            dead = False
+        if paused == True:
+            paused = False
+            canvas.coords(box, WIDTH + 5, HEIGHT + 5, WIDTH + 10, HEIGHT + 10)
+            canvas.coords(text, WIDTH + 50, HEIGHT + 50)
+        if in_a_jump == True:
+            in_a_jump = False
+            jump_index = 0
+        score = 0
+        canvas.coords(cactus1, WIDTH - 200, HEIGHT - 110)
+        canvas.coords(cactus2, WIDTH, HEIGHT - 110)
+        canvas.coords(cactus3, WIDTH + 200, HEIGHT - 110)
+        canvas.coords(airplane1, WIDTH + 100, 250)
+        canvas.coords(airplane2, -100, 100)
+        canvas.coords(dino_animation, 150, HEIGHT - 120)
+        canvas.coords(d_box, WIDTH + 5, HEIGHT + 5, WIDTH + 10, HEIGHT + 10)
+        canvas.coords(d_text, WIDTH + 50, HEIGHT + 50)
+        canvas.coords(rip1, 0, HEIGHT + 100)
+        canvas.coords(rip2, 0, HEIGHT + 100)
+        canvas.coords(rip3, 0, HEIGHT + 100)
+        canvas.itemconfig(dino_animation, image=frames[0])
+    if (event.char == 's' or event.char == 'S' ) and beginning == True:
+        beginning = False
+        canvas.delete(main_menu)
+
+
+# use some standard variables.
+WIDTH = 800
+HEIGHT = 600
+Title = "Dino Run"
+
+score = 0 # will be used to count the points
+highscore = 0
+
+DELAY = 100 # will be used to update the score based on this time (ms)
+FRAME_COUNT = 8
+frame_index = 0 # the index of the current frame - used to drive the animation
+speed = 20
+distance = 200
+
+in_a_jump = False  # this boolean values tells us when already in a jump as to not start another one
+jump_offsets = [0, -65, -40, -25, 20, 45, 65]
+jump_index = 0 # the jump index is used to keep track of the phase of the jump, it completes after len(jump_offsets) steps
+
+
+# speeds up based on score
+if score > 100:
+    speed += 5
+if score > 200:
+    speed += 5
+if score > 300:
+    speed += 5
+if score > 400:
+    speed += 5
+
+
+#create the window
+win = Tk()
+win.title(Title)
+win.geometry(str(WIDTH + 10)+"x"+str(HEIGHT+10))
+
+
+# makes the window not resizeable
+win.resizable(False, False) # [https://www.tutorialspoint.com/how-to-make-a-tkinter-window-not-resizable#:~:text=Let%20us%20suppose%20that%20we,resizable(False%2C%20False).]
+
+
+# create canvas and frame
+canvas = Canvas(win, width=WIDTH, height=HEIGHT)
+canvas.pack()
+
+
+# creates sky and list for the colors
+sky = ['cyan','skyblue','blue','darkblue']
+sky_box = canvas.create_rectangle(-10,-10,WIDTH+10,HEIGHT, fill=sky[0])
+
+
+# add pictures to the game
+background_img = PhotoImage(file='resources/background-tall.png')
+background = canvas.create_image(WIDTH/2, HEIGHT/2, image= background_img)
+
+ground_img = PhotoImage(file='resources/cityground.png')
+ground = canvas.create_image(WIDTH/2, HEIGHT-45, image= ground_img)
+
+airplane1_img = PhotoImage(file='resources/airplane1.png')
+airplane2_img = PhotoImage(file='resources/airplane2.png')
+airplane1 = canvas.create_image(WIDTH + 100, 250, image=airplane1_img)
+airplane2 = canvas.create_image(-100, 100, image=airplane2_img)
+
+frames = [PhotoImage(file='resources/hulk%i.png' % i) for i in range(FRAME_COUNT+1)]
+dead_frame = PhotoImage(file='resources/hulk9.png')
+dino_animation = canvas.create_image(150, HEIGHT-120, image=frames[0])
+
+cac1 = PhotoImage(file='resources/building-tall.png')
+cac2 = PhotoImage(file='resources/building-double.png')
+cac3 = PhotoImage(file='resources/building-tall.png')
+cactus1 = canvas.create_image(WIDTH - 200, HEIGHT-110, image=cac1)
+cactus2 = canvas.create_image(WIDTH , HEIGHT-110, image=cac2)
+cactus3 = canvas.create_image(WIDTH + 200, HEIGHT-110, image=cac3)
+
+rip1_img = PhotoImage(file='resources/building-tall-rip.png')
+rip2_img = PhotoImage(file='resources/building-double-rip.png')
+rip3_img = PhotoImage(file='resources/building-tall-rip.png')
+rip1 = canvas.create_image(0,HEIGHT + 100, image= rip1_img)
+rip2 = canvas.create_image(0,HEIGHT + 100, image= rip2_img)
+rip3 = canvas.create_image(0,HEIGHT + 100, image= rip3_img)
+
+main_menu_img = PhotoImage(file='resources/main-menu.png')
+
+
+# for pausing
+paused = False
+box = canvas.create_rectangle(WIDTH+5, HEIGHT+5, WIDTH+10, HEIGHT+10, fill= 'cyan', width= 5 )
+text = canvas.create_text(WIDTH + 50 , HEIGHT + 50, text='PAUSED', font=('System', 20), fill='black')
+
+# for end screen
+dead = False
+d_box = canvas.create_rectangle(WIDTH+5, HEIGHT+5, WIDTH+10, HEIGHT+10, fill= 'red', width= 5 )
+d_text = canvas.create_text(WIDTH + 50 , HEIGHT + 50, text='GAME OVER', font=('System', 20), fill='black')
+
+
+# creates the small guide on the bottom left of the screen and the score on the bottom right and the highscore on top right
+canvas.create_text(200, HEIGHT - 10, text= 'CO1417  -  Dino Run  |  Q: Quit  -  P: Pause  -  R: Restart', font= ("System",15), fill= 'black')
+s_text = canvas.create_text(WIDTH - 50, HEIGHT - 10, text= f'{score:08}', font= ("System",15), fill= 'black') # [https://bobbyhadz.com/blog/python-add-leading-zeros-to-number]
+h_text = canvas.create_text(WIDTH - 50, 10, text= f'HI {highscore:08}', font= ("System",15), fill= 'black')
+
+
+# create a main menu for the beginning
+beginning = True
+main_menu = canvas.create_image(WIDTH/2, HEIGHT/2, image=main_menu_img)
+
+
+win.after(0, update_all) # calls function cimutaniusly
+
+
+update(jump_index) # calls function
+
+
+# binds functions
+win.bind('<KeyPress>', key_press)
+
+win.bind("<space>", jump)
+
+
+# keeps the window up
+win.mainloop()
